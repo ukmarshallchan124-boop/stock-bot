c3bf74a5469dc669d5dfeddcd8f9bc526f2b4946
ukmarshallchan124-boop <ukmarshallchan124@gmail.com>
2026-04-20 23:21:48 +0100

    Update bot.py

--- bot.py
+++ bot.py
@@ -318,8 +318,8 @@ def loop():
 def stock_all():
     allow, market_msg = market_filter()
 
-    header = "🟢 市場偏多" if allow else "🔴 市場偏弱"
-
+    header = "🟢 市場偏多（可進攻）" 
+    if allow else "🔴 市場偏弱（保守）"
     msg = f"""📊【市場掃描 Pro】
 {market_msg}
 {header}
