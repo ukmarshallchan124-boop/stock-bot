0fe198c325c788566aa79e78285d769d69177512
ukmarshallchan124-boop <ukmarshallchan124@gmail.com>
2026-04-20 23:22:36 +0100

    Update bot.py

--- bot.py
+++ bot.py
@@ -36,8 +36,8 @@ def signal_engine(df, d):
 
     vol = df["Volume"]
     vol_ma = vol.rolling(10).mean().iloc[-1]
-    volume_spike = Falseif    
-    if vol_ma and not pd.isna(vol_ma):
+    volume_spike = False
+if vol_ma is not None and not pd.isna(vol_ma):
     volume_spike = vol.iloc[-1] > vol_ma * 1.5
     
     breakout = (
